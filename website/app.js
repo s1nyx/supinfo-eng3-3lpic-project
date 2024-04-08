5f396e89cb66978921a78b10e2839bccf630cabb
s1nyx <sinyx.contact@gmail.com>
2024-04-08 17:04:55 +0200

    tweak: test version

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
 const pool = mysql.createPool({
     connectionLimit : 10,
