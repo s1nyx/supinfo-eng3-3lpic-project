be4f6c6464cfa7c96df796f90680fb43aa68cc02
s1nyx <sinyx.contact@gmail.com>
2024-04-08 15:55:17 +0200

    docs: site.pp and bdd.pp are good and working

--- website/app.js
+++ website/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
+const port = 3000;
 
+// Configuration de la connexion à la base de données
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'db1.localdomain.lan',
+    user            : 'replication_user',
+    password        : 'strong_replication_password',
+    database        : 'your_database_name'
+});
+
+// Route principale qui affiche des données depuis la base de données
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+    pool.query('SELECT * FROM votre_table LIMIT 10', (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
 
-app.listen(9000, () => {
-    console.log('Server is running on port localhost:3000');
-})
+app.listen(port, () => {
+    console.log(`L'application écoute sur le port ${port}`);
+});
